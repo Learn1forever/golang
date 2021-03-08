1bcdddcbbbc18a0f7007ffdf2d01438170ba745c
nieda <nieda@zving.com>
2021-03-08 17:32:18 +0800

    random greetings

--- lesson01/greetings/greetings.go
+++ lesson01/greetings/greetings.go
@@ -3,7 +3,24 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
+
+//init
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+//
+func randomFormat() string {
+	formats := []string{
+		"Hi,%v. Welcome!",
+		"Great to see you, %v!",
+		"Hail,%v! Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
+}
 
 //
 
@@ -12,6 +29,6 @@ func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
